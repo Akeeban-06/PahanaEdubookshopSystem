d51eaf83ea3c98e761cf5d984e153f0a59be5efd
Akeeban-06 <logeesanakeeban2002@gmail.com>
2025-08-03 23:03:32 +0530

    Refactor item

--- src/java/com/pahanaedu/model/Item.java
+++ src/java/com/pahanaedu/model/Item.java
@@ -1,52 +1,32 @@
 package com.pahanaedu.model;
 
+import java.math.BigDecimal;
 
 public class Item {
     private int itemId;
     private String itemName;
-    private double price;
+    private BigDecimal price;
     private int stock;
     
-    // Default constructor
+    // Constructors
     public Item() {}
     
-    // Parameterized constructor
-    public Item(String itemName, double price, int stock) {
+    public Item(String itemName, BigDecimal price, int stock) {
         this.itemName = itemName;
         this.price = price;
         this.stock = stock;
     }
     
     // Getters and Setters
-    public int getItemId() {
-        return itemId;
-    }
+    public int getItemId() { return itemId; }
+    public void setItemId(int itemId) { this.itemId = itemId; }
     
-    public void setItemId(int itemId) {
-        this.itemId = itemId;
-    }
+    public String getItemName() { return itemName; }
+    public void setItemName(String itemName) { this.itemName = itemName; }
     
-    public String getItemName() {
-        return itemName;
-    }
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
     
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-    
-    public double getPrice() {
-        return price;
-    }
-    
-    public void setPrice(double price) {
-        this.price = price;
-    }
-    
-    public int getStock() {
-        return stock;
-    }
-    
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
 }
